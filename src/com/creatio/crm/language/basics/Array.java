da064c472f36001d5fc253c5a166bd1688c81c33
Vinay Kumar <mudravinaykumar@gmail.com>
2024-12-23 17:46:00 +0530

    First Commit

---
+++ src/com/creatio/crm/language/basics/Array.java
@@ -0,0 +1,17 @@
+package com.creatio.crm.language.basics;
+
+public class Array {
+
+	public static void main(String[] args) {
+		String[] empName=new String[3];
+		empName[0]="Vinay";
+		empName[1]="Vijay";
+		empName[2]="Anil";
+		//empName[3]="Ram";
+		System.out.println(empName[0]);
+		
+		
+
+	}
+
+}
